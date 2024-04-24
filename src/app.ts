48642f7c772654a0e38007aeaeecc5065e1fd0c1
Chetan-jadhav <jadhavchetan2506@gmail.com>
2024-04-24 21:47:35 +0530

    Add global error handler.

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import createHttpError, { HttpError } from "http-errors";
+import { config } from "./config/config";
 
 const app = express();
 
 app.get("", (req, res, next) => {
+  const error = createHttpError(400, "something went wrong");
+  throw error;
   res.json({ message: "Welcome to rest apis" });
 });
 
