66794498412d4fe0ae28bb50c2aafa86540a232a
Chetan-jadhav <jadhavchetan2506@gmail.com>
2024-04-26 23:09:12 +0530

    Create user router

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,17 @@
 import express, { NextFunction, Request, Response } from "express";
 import createHttpError, { HttpError } from "http-errors";
 import { config } from "./config/config";
+import userRouter from "./users/userRouter";
 
 const app = express();
+app.use(express.json());
 
 app.get("", (req, res, next) => {
   const error = createHttpError(400, "something went wrong");
   throw error;
   res.json({ message: "Welcome to rest apis" });
 });
+
+app.use("/api/users", userRouter);
 
 export default app;
