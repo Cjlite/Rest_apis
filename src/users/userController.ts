e0fb558ab597949499577664b1194b07da3cf96a
Chetan-jadhav <jadhavchetan2506@gmail.com>
2024-04-27 12:47:56 +0530

    Add error handling

--- src/users/userController.ts
+++ src/users/userController.ts
@@ -2,6 +2,9 @@ import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
 import userModal from "./userModal";
 import bcrypt from "bcrypt";
+import { sign } from "jsonwebtoken";
+import { config } from "../config/config";
+import { User } from "./userTypes";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   const { name, email, password } = req.body;
@@ -11,16 +14,39 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     return next(error);
   }
 
-  const user = await userModal.findOne({ email });
+  try {
+    const user = await userModal.findOne({ email });
 
-  if (user) {
-    const error = createHttpError(404, "User already exits with this email");
-    return next(error);
+    if (user) {
+      const error = createHttpError(404, "User already exits with this email");
+      return next(error);
+    }
+  } catch (error) {
+    return next(createHttpError(400, "Error while getting error"));
   }
 
   const hashPassword = await bcrypt.hash(password, 10);
 
-  res.json({ message: "User created" });
+  let newUser: User;
+  try {
+    newUser = await userModal.create({
+      name,
+      email,
+      password: hashPassword,
+    });
+  } catch (error) {
+    return next(createHttpError(500, "Error while creating error"));
+  }
+
+  try {
+    const token = sign({ sub: newUser._id }, config.jwtSecret as string, {
+      expiresIn: "7d",
+    });
+
+    res.json({ accessToken: token });
+  } catch (error) {
+    return next(createHttpError(500, "Error while signing the jwt token"));
+  }
 };
 
 export { createUser };
